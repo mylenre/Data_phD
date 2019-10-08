61cdd571aa7c9ba239883be6239f526912d5463f
mylenre <mylenereceveur@gmail.com>
2019-10-08 15:05:27 +0100

    create first script

---
+++ intro_R/intro_R_script.R
@@ -0,0 +1,15 @@
+# Numerical Modeling course1 - R Basics
+# Learning how to import and explore data, and make graphs about Edinburgh's biodiversity
+# Written by Mylene Receveur 07/10/2019 University of Edinburgh
+
+#install.packages("package-name")
+#library(package-name)
+# dplyr package to provide extra commands for formatting and manipulating data
+
+install.packages("dplyr")
+library(dplyr)
+# Note that there are quotation marks when installing a package, but not when loading it
+ 
+#getwd()
+setwd("D:/mylen/Documents/phD/Data_phD/intro_R") 
+#ls()
