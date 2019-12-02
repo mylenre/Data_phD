015cac4f0023ffc0cb097c787f33d00007ea5c50
mylenre <55740158+mylenre@users.noreply.github.com>
2019-12-02 13:02:59 +0000

    Update read_profile.py
    Add subplots

--- Scripts/read_profile.py
+++ Scripts/read_profile.py
@@ -7,6 +7,18 @@ Created on Thu Nov 21 09:23:13 2019
 import matplotlib.pyplot as plt
 import numpy as np
 
+def moving_average(y, K=5):
+    """
+    2K+1 point moving average of array y
+    """
+    N = np.size(y)      # find the size of y
+    s = np.zeros(N)     # make an array of zeros with the same size
+    for n in range(N):  # loop for the moving average
+        kmin = max(n-K, 0)    # limit to indices within the array
+        kmax = min(n+K+1, N)  # i.e. range 0 to N-1
+        s[n] = np.mean(y[kmin:kmax])
+    return s            # return the smoothed array
+
 time = []
 z = []
 T= []
@@ -30,16 +42,19 @@ with open('M2_ply_PROD_WELL_t2.tec', 'r') as file:
        z.append(float(this_line[0].rstrip()))
        T.append(float(this_line[1].rstrip()))
        data = [z,T]
-#IDmaxz= z.index(np.max(z)
 IDmaxz = []
 for i in range(0, len(z)) : 
     if z[i] == np.max(z): 
-        IDmaxz.append(i)       
-IDsurf = []
+        IDmaxz.append(i) 
+ID98 = []			#define depth for plotting T change over time
 for i in range(0, len(z)) : 
+    if z[i] == 98.5: 
+        ID98.append(i)       
+IDsurf = []
+for i in range(0, len(z)) :  
     if z[i] == 0: 
-        IDsurf.append(i) 
-IDsurf=IDsurf[0::2];
+        IDsurf.append(i)         #identify new time step
+IDsurf=IDsurf[0::2];             # remove doublons
 
 #nbplot=len(z)/(id+1)
 z= np.array(z) 
@@ -47,22 +62,45 @@ T= np.array(T)
 time=np.array(time)
 time_yrs=time/(3600*24*365)
 
-plt.figure(figsize=(10,10))
+plt.figure(figsize=(20,10))
+plt.subplot(1,2,1)
 plt.rcParams['font.size'] = 12
-for i in range(0, np.size(time),10):
+for i in range(0, np.size(time),30): # change increment not to plot all time-step profiles
     plt.plot(T[IDsurf[i]:IDmaxz[i]],-z[IDsurf[i]:IDmaxz[i]], label=time[i])     
 plt.xlabel('Temperature')
 plt.ylabel('Depth borehole')
 plt.legend(loc='best')
 plt.legend(fontsize=12)  
-plt.title('Change in sub-surface temperature with time (sec) for a top BC: T=10 C')
+plt.title('Change in sub-surface temperature with time (s) for a top BC: T=10 C')
+
+plt.subplot(1,2,2)
+for i in range(0, np.size(time),30): # change increment not to plot all time-step profiles
+    T_smooth = moving_average(T[IDsurf[i]:IDmaxz[i]], K=5)
+    plt.plot(T_smooth, -z[IDsurf[i]:IDmaxz[i]], lw=1,label=time[i])   
+plt.xlabel('Temperature')
+plt.ylabel('Depth borehole')
+plt.legend(loc='best')
+plt.legend(fontsize=12)  
+plt.title('Average change in temperature by 5 nodes intervals')
+
 plt.savefig("Tprofiles.png") 
 
-plt.figure(figsize=(10,5))
+plt.figure(figsize=(10,10))
+plt.subplot(2,1,1)
+plt.plot(time_yrs,T[ID98])
+plt.xlabel('Time (years)')
+plt.ylabel('Temperature at ~98m depth')
+plt.legend(loc='best')
+plt.legend(fontsize=12)  
+plt.title('Change in temperature with time')
+#plt.savefig("T98m.png")
+
+plt.subplot(2,1,2)
 plt.plot(time_yrs,T[IDmaxz])
 plt.xlabel('Time (years)')
-plt.ylabel('Temperature at ~100m depth')
+plt.ylabel('Bottom Hole temperature')
 plt.legend(loc='best')
 plt.legend(fontsize=12)  
 plt.title('Change in BTH with time')
-plt.savefig("BTH.png") 
+#plt.savefig("BTH.png") 
+plt.savefig("Tchange.png") 
