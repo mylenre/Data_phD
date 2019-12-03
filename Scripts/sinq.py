03cfe3b01dcd7df89db2c06539e97ea4ce859301
mylenre <55740158+mylenre@users.noreply.github.com>
2019-12-03 15:15:52 +0000

    Update

--- Scripts/sinq.py
+++ Scripts/sinq.py
@@ -8,23 +8,24 @@ Created on Fri Nov 29 01:12:49 2019
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-os.chdir(r'c:\Users\s1995204\Documents_LOCAL\Modeling\Modeling_results\1D_models\input_files')
+os.chdir(r'C:\Users\s1995204\Documents_LOCAL\Modeling\Modeling_results\Solar_1D_models\input_files')
+
+qmax=input("Enter qmax : ")
+qmin=input("Enter qmin : ")
+amp=(abs(eval(qmax))+abs(eval(qmin)))/2
 
 dt_d=1
 dt_s=3600*24
 
-t_y=3
+t_y=input("Enter nb year : ")
+t_y=eval(t_y)
 t_d=dt_d*365.25*t_y
 t_s=dt_s*365.25*t_y
-
-qmax=250
-ref=50
-#name=ref=qmax-1/2*qmax
 
 
 time = np.arange(0, t_d, dt_d)
 time_s= np.arange(0, t_s, dt_s)
-amplitude   = ref-qmax*np.cos(2*np.pi*(time/t_d*t_y))
+amplitude = (amp+eval(qmin))-amp*np.cos(2*np.pi*(time/t_d*t_y))
 
 plt.plot(time, amplitude)
 plt.title('Sine wave')
@@ -35,5 +36,5 @@ plt.axhline(y=0, color='k')
 plt.show()
 
 data=np.stack((time_s, amplitude), axis=-1)
-np.savetxt('sinq_'+str(ref)+'_'+str(qmax)+'.txt', data)
+np.savetxt('sinq_'+ qmin +'_'+qmax+'.txt', data)
 
